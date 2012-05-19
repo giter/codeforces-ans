88dc8221c4be51686a0f22bc2ff4cc76cbc7e33d
giter <nubix@qq.com>
2012-05-19 11:21:52 +0800

    Add an TLE version of 190C

--- src/190C.hs
+++ src/190C.hs
@@ -1,7 +1,18 @@
 module Main where
 
+f :: [Int] -> [[Char]] -> [[Char]] -> [Char]
+f (2:2:1:xs) (a:b:c:ys) d = f (2:xs) ((c++"<"++b++","++a++">"):ys) d
+f (2:1:xs) ys ("int":d) = f (2:2:1:xs) ("int":ys) d
+f (2:1:xs) ys ("pair":d) = f (1:2:1:xs) ("pair":ys) d
+f (1:xs) ys ("int":d) = f (2:1:xs) ("int":ys) d
+f (1:xs) ys ("pair":d) = f (1:1:xs) ("pair":ys) d
+f (2:[]) [y] [] = y
+f [] [] ("int":d) = f (2:[]) ("int":[]) d
+f [] [] ("pair":d) = f (1:[]) ("pair":[]) d
+f [] [] [] = ""
+f _ _ _ = "Error occurred"
 
-f (x:xs) n 
-    | x == "pair" = "pair<" ++ (f xs (n+1)) ++ ">"
-    | x == "int"  = "int," ++  (f xs (n-1))
-f [] n = ""
+main = do
+    a <- getLine
+    b <- getLine
+    putStrLn $ f [] [] (words b)
