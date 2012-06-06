4246598fca69104bb1f3b37361ad16e291ed2a6b
giter <nubix@qq.com>
2012-06-06 22:33:40 +0800

    add 1B.hs

--- src/1B.hs
+++ src/1B.hs
@@ -12,13 +12,5 @@ f n
 f' [] = 0
 f' p  = (ord $ last p ) - (ord 'A') + 1 + 26 * (f' $ init p)
 
-idx c [] = -1
-idx c (x:xs) = 
-    idx' c (x:xs) 0
-    where
-        idx' c (x:xs) n = if x == c then n else idx' c xs (n+1)
-        idx' c [] n = -1
- 
-check str = if (idx 'R' str == 0) && (idx 'R' str) > ((idx 'C' str)+1) then (splitAt $ idx 'C' str )  else 
 
-    
+
