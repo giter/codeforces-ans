7b0036f2d54b45282303ba5594d4784f42735ec4
nubix <nubix@qq.com>
2013-06-25 00:16:21 +0800

    Accepted version of 2A.go

---
+++ src/2/2A.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  m := make(map[string]int,1000)
+  ln := make([]string,1000)
+  ls := make([]int,1000)
+  var n string
+  var s int
+  var t int
+  fmt.Scanln(&t)
+
+    ms := int(-2000*1000) //max score
+
+  for i:=0 ; i<t ; i++ {
+    
+    fmt.Scanln(&n,&s)
+
+    ln[i] = n
+    ls[i] = s
+
+    m[n] += s
+  }
+
+  for _,v := range m {
+    if v>ms { ms = v }
+  }
+
+  k := make(map[string]int,1000)
+
+  for i:=0;i<t;i++ {
+
+    n = ln[i] 
+    s = ls[i]
+
+    k[n] += s
+
+    if k[n] >= ms && m[n] == ms {
+      fmt.Println(n)
+      return;
+    }
+
+  }
+
+}
